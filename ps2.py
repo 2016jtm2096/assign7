8d1a8cc4a0c73f507da73a238eeeb1af205de61b
2016jtm2096 <dheeraj2012079@gmail.com>
2016-09-12 15:16:17 +0530

    update

--- ps2.py
+++ ps2.py
@@ -1,3 +1,16 @@
-###### this is the second .py file ###########
-
-####### write your code here ##########
+import random
+#asking to enter total number of users in that area
+x=input("enter total number of user in that area: ")
+#generating random location of users in that area
+numbers = {random.random()*2-1:random.random()*2-1  for i in range(x)}
+print numbers
+y = numbers.keys()
+z = numbers.values()
+count=0
+#counting the number of people who are outside the circle
+for i in range(x) :
+ g=y[i]*y[i]+z[i]*z[i]
+ if(g>1) :
+  count=count+1
+t=count*100/x
+print  "percentange outside",t,"%"
